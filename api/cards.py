108bcb7dadb616b11af2d4c7d25223c4b1853e6e
zliel <zpliel@gmail.com>
2022-12-21 19:34:07 -0800

    Added a create and read functionalities for the card collection

---
+++ api/cards.py
@@ -0,0 +1,34 @@
+import dotenv
+
+from models import Card
+
+from fastapi import APIRouter
+from pymongo import MongoClient
+
+router = APIRouter(prefix="/api/cards", tags=["cards"])
+mongo_host = dotenv.get_key(".env", "MONGO_HOST")
+client = MongoClient(mongo_host)
+db = client['lingua-tile']
+card_collection = db['cards']
+
+
+@router.get("/")
+async def get_all_cards():
+    """Retrieve all cards from the database"""
+    cards = card_collection.find()
+    return [Card(**card) for card in cards]
+
+
+@router.get("/create/{front_text}/{back_text}/{lesson_id}")
+async def create_card(front_text, back_text, lesson_id):
+    """Create a new card in the database"""
+    new_card = Card(front_text, back_text, lesson_id)
+    card_collection.insert_one(new_card.__dict__)
+    return new_card
+
+
+@router.get("/{card_id}")
+async def get_card(card_id):
+    """Retrieve a card from the database by id"""
+    card = card_collection.find_one({"_id": card_id})
+    return card
