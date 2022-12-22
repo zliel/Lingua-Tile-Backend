0ba8111264a8fe6880ec004d401d5adaab1bbe44
zliel <zpliel@gmail.com>
2022-12-21 23:11:51 -0800

    Moved the routes regarding translations into the api module

---
+++ api/translations.py
@@ -0,0 +1,53 @@
+import dotenv
+from fastapi import APIRouter
+import requests
+
+router = APIRouter(prefix="/api/translations", tags=["translations"])
+host = "deep-translate1.p.rapidapi.com"
+api_key = dotenv.get_key(".env", "API_KEY")
+headers = {
+    "x-rapidapi-host": host,
+    "x-rapidapi-key": api_key,
+    "content-type": "application/json",
+}
+
+payload = {
+    "q": "Hello, world!",
+    "source": "en",
+    "target": "es"
+}
+
+
+@router.get("/{src_text}/{src_lang}/{target_lang}")
+async def translate(src_text, src_lang, target_lang):
+    """Translate text from one language to another"""
+    url = f"https://{host}/language/translate/v2"
+    querystring = {"q": src_text, "target": target_lang, "source": "en"}
+    response = requests.request("POST", url, headers=headers, json=querystring)
+
+    translation_container = response.json()["data"]["translations"]
+    translation = translation_container
+
+    translation_response = {
+        "sourceText": src_text,
+        "sourceLanguage": src_lang,
+        "translatedText": translation["translatedText"],
+        "targetLanguage": target_lang,
+    }
+
+    return translation_response
+
+
+@router.get("/languages")
+async def get_languages():
+    # url = f"https://{host}/language/translate/v2/languages"
+    # response = requests.request("GET", url, headers=headers)
+    # filter the response to only return english and japanese
+
+    # languages = response.json()["languages"]
+    # languages = [lang for lang in languages if lang["language"] in ["en", "ja"]]
+    # Calling the deep-translate API to get a list of languages is costly, so we'll just return English and Japanese
+    # until I figure out whether I want to cache the results or save them in a MongoDB collection
+
+    languages = [{"language": "en", "name": "English"}, {"language": "ja", "name": "Japanese"}]
+    return languages
