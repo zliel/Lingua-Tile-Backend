db4d78127ada409e94ba66577967dcf28a4880ed
zliel <zpliel@gmail.com>
2022-12-23 16:03:36 -0800

    Changed the Lesson class to use Pydantic for validation, and to include it in the documentation

--- models/lessons.py
+++ models/lessons.py
@@ -1,8 +1,21 @@
+from typing import List
 from bson.objectid import ObjectId
+from pydantic import BaseModel, Field
+from models.py_object_id import PyObjectId
 
 
-class Lesson:
-    def __init__(self, name: str, cards: list = None, _id: str = None):
-        self.name = name
-        self.cards = cards if cards else []
-        self._id = _id if _id else str(ObjectId())
+class Lesson(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    cards: List[str] = Field(default_factory=list)
+
+    class Config:
+        arbitrary_types_allowed = True
+        allow_population_by_field_name = True
+        json_encoders = {ObjectId: lambda oid: str(oid)}
+        schema_extra = {
+            "example": {
+                "name": "Greetings in Japanese",
+                "cards": ["5f9f1b9b9c9d1c0b8c8b9c9d"]
+            }
+        }
