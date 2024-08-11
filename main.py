b47a9d1bdf5d550cae33ddd6c9bde2ab7da63db6
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-11 16:07:00 -0700

    Added the auth and user routers to the app

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.cards import router as cards_router
 from api.lessons import router as lessons_router
 from api.translations import router as translations_router
+from api.auth import router as auth_router
+from api.users import router as users_router
 
 title = "Japanese Flashcards API"
 description = "An API for creating and studying Japanese flashcards"
@@ -10,6 +12,8 @@ app = FastAPI(title=title, description=description, version="0.1.0")
 app.include_router(cards_router)
 app.include_router(lessons_router)
 app.include_router(translations_router)
+app.include_router(auth_router)
+app.include_router(users_router)
 origins = ["http://localhost:3000"]
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_methods=["*"])
 
