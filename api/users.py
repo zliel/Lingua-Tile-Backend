16daada58f5ccd5592a979d30cdc29cc1d855f32
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-12 16:01:03 -0700

    Added helper functions to get the current user from the JWT token and determine whether the user is an admin

--- api/users.py
+++ api/users.py
@@ -3,7 +3,9 @@ from fastapi.encoders import jsonable_encoder
 
 from models import User
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+import jwt
 from pymongo import MongoClient
 
 router = APIRouter(prefix="/api/users", tags=["Cards"])
@@ -11,6 +13,33 @@ mongo_host = dotenv.get_key(".env", "MONGO_HOST")
 client = MongoClient(mongo_host)
 db = client['lingua-tile']
 user_collection = db['users']
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+SECRET_KEY = dotenv.get_key(".env", "SECRET_KEY")
+ALGORITHM = "HS256"
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        # Decode the token to get the username
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+
+        # Find the user in the database by username
+        user = user_collection.find_one({"username": username})
+        if user is None:
+            raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+
+        return User(**user)
+
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+
+
+def is_admin(user: User):
+    return "admin" in user.roles
 
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
