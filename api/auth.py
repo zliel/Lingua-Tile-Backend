e804a22b24b0208f64433f44cf8b5566195cc082
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-11 18:28:18 -0700

    Used passlib to handle encrypting and verifying passwords

--- api/auth.py
+++ api/auth.py
@@ -1,22 +1,31 @@
-import bson
 import dotenv
-from fastapi.encoders import jsonable_encoder
 
 from models import User
 
 from fastapi import APIRouter, status, HTTPException
 from pymongo import MongoClient
+from passlib.context import CryptContext
 
 router = APIRouter(prefix="/api/auth", tags=["Auth"])
 mongo_host = dotenv.get_key(".env", "MONGO_HOST")
 client = MongoClient(mongo_host)
 db = client['lingua-tile']
 user_collection = db['users']
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 @router.post("/login", status_code=status.HTTP_200_OK)
 async def login_user(user: User):
     """Login a user"""
-    user = user_collection.find_one({"username": user.username, "password": user.password})
-    if user is None:
+    found_user = user_collection.find_one({"username": user.username})
+
+    if found_user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    return user
+    elif not pwd_context.verify(user.password, found_user['password']):
+        raise HTTPException(status_code=401, detail="Incorrect password")
+
+    # Convert found_user to User model and remove its password from the response
+    found_user = User(**found_user)
+    del found_user.password
+
+    return found_user
