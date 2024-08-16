9c8e83403dec5a9c931c00a954d5ea0b24e21f2c
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-15 21:51:38 -0700

    Added a Section model to group together lessons

---
+++ models/sections.py
@@ -0,0 +1,20 @@
+from typing import List
+from pydantic import BaseModel, Field
+from models.py_object_id import PyObjectId
+from bson.objectid import ObjectId
+
+class Section(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    name: str = Field(...)
+    lesson_ids: List[PyObjectId] = Field(default_factory=list)
+
+    class Config:
+        arbitrary_types_allowed = True
+        allow_population_by_field_name = True
+        json_encoders = {ObjectId: lambda oid: str(oid)}
+        schema_extra = {
+            "example": {
+                "name": "Kana",
+                "lesson_ids": ["5f9f1b9b9c9d1c0b8c8b9c9d"]
+            }
+        }
