60eb2acc940408fd02f439a556a4c27bf06dd48b
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-15 21:48:08 -0700

    Updated the Card model to use "lesson_ids" instead of "lesson_id"

--- models/cards.py
+++ models/cards.py
@@ -10,7 +10,7 @@ class Card(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     front_text: str = Field(...)
     back_text: str = Field(...)
-    lesson_id: List[str] = Field(...)
+    lesson_ids: List[str] = Field(...)
 
     class Config:
         arbitrary_types_allowed = True
@@ -20,6 +20,6 @@ class Card(BaseModel):
             "example": {
                 "front_text": "Hello",
                 "back_text": "こんにちは",
-                "lesson_id": ["5f9f1b9b9c9d1c0b8c8b9c9d"]
+                "lesson_ids": ["5f9f1b9b9c9d1c0b8c8b9c9d"]
             }
         }
