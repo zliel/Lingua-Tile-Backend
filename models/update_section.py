999625f3e41059ea8737632300237de31f740e93
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-18 04:11:29 -0700

    Updated the UpdateSection model to use PyObjectId

---
+++ models/update_section.py
@@ -0,0 +1,19 @@
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+from .py_object_id import PyObjectId
+
+class UpdateSection(BaseModel):
+    name: Optional[str] = None
+    lesson_ids: Optional[List[PyObjectId]] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "name": "Hello",
+                "lesson_ids": ["5f9f1b9b9c9d1c0b8c8b9c9d"]
+            }
+        }
