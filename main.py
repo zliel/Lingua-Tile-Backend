263aeacc589b805e9c7d1408e98475ed3b5871f8
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-24 16:40:13 -0700

    Added a more helpful error message for unprocessable entity errors

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
+import logging
+
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse
+
 from api.cards import router as cards_router
 from api.lessons import router as lessons_router
 from api.translations import router as translations_router
@@ -29,3 +34,10 @@ app.add_middleware(
 @app.get("/", tags=["Root"])
 async def root():
     return {"message": "Hello World"}
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+	exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+	logging.error(f"{request}: {exc_str}")
+	content = {'status_code': 422, 'message': exc_str, 'data': None}
+	return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
