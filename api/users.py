8c571f1907ffc4076e4b374572c9c16bd092c263
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-28 23:39:23 -0700

    Updated the user routes to use dep. injection for the db

--- api/users.py
+++ api/users.py
@@ -1,43 +1,12 @@
 import dotenv
-from fastapi.encoders import jsonable_encoder
 from passlib.context import CryptContext
 
+from api.dependencies import get_db, get_current_user, pwd_context
 from models import User, PyObjectId, UpdateUser
 
 from fastapi import APIRouter, status, HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer
-import jwt
-from pymongo import MongoClient
 
 router = APIRouter(prefix="/api/users", tags=["Users"])
-mongo_host = dotenv.get_key(".env", "MONGO_HOST")
-client = MongoClient(mongo_host)
-db = client['lingua-tile']
-user_collection = db['users']
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-SECRET_KEY = dotenv.get_key(".env", "SECRET_KEY")
-ALGORITHM = "HS256"
-
-
-def get_current_user(token: str = Depends(oauth2_scheme)):
-    try:
-        # Decode the token to get the username
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-
-        if username is None:
-            raise HTTPException(status_code=401, detail="Invalid authentication credentials")
-
-        # Find the user in the database by username
-        user = user_collection.find_one({"username": username})
-        if user is None:
-            raise HTTPException(status_code=401, detail="Invalid authentication credentials")
-
-        return User(**user)
-
-    except jwt.PyJWTError:
-        raise HTTPException(status_code=401, detail="Invalid authentication credentials")
 
 
 def is_admin(user: User):
@@ -45,8 +14,10 @@ def is_admin(user: User):
 
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
-async def create_user(user: User):
+async def create_user(user: User, db=Depends(get_db)):
     """Create a new user in the database"""
+    user_collection = db['users']
+
     if user_collection.find_one({"username": user.username}):
         raise HTTPException(status_code=400, detail="Username already exists")
 
@@ -65,21 +36,24 @@ async def get_current_user(current_user: User = Depends(get_current_user)):
 
 
 @router.get("/{user_id}", response_model=User, response_model_exclude={"password"})
-async def get_user(user_id: PyObjectId, current_user: User = Depends(get_current_user)):
+async def get_user(user_id: PyObjectId, current_user: User = Depends(get_current_user), db=Depends(get_db)):
     """Retrieve a user from the database by id"""
     if not is_admin(current_user) and current_user.id != user_id:
         raise HTTPException(status_code=403, detail="Not authorized to view this user")
+
+    user_collection = db['users']
 
     user = user_collection.find_one({"_id": user_id})
     return User(**user)
 
 
 @router.get("/admin/all", response_model=list[User], response_model_exclude={"password"})
-async def get_all_users(current_user: User = Depends(get_current_user)):
+async def get_all_users(current_user: User = Depends(get_current_user), db=Depends(get_db)):
     """Retrieve all users from the database"""
     if not is_admin(current_user):
         raise HTTPException(status_code=403, detail="Not authorized to view all users")
 
+    user_collection = db['users']
     users = user_collection.find()
     user_list = []
     for user in users:
@@ -92,7 +66,7 @@ async def get_all_users(current_user: User = Depends(get_current_user)):
 
 
 @router.put("/update/{user_id}", response_model=User, response_model_exclude={"password"})
-async def update_user(user_id: PyObjectId, updated_info: UpdateUser, current_user: User = Depends(get_current_user)):
+async def update_user(user_id: PyObjectId, updated_info: UpdateUser, current_user: User = Depends(get_current_user), db=Depends(get_db)):
     """Update a user in the database by id"""
     user_info_to_update = {k: v for k, v in updated_info.dict().items() if v is not None}
 
@@ -101,10 +75,11 @@ async def update_user(user_id: PyObjectId, updated_info: UpdateUser, current_use
             user_info_to_update["password"] = pwd_context.hash(user_info_to_update["password"])
         else:
             del user_info_to_update["password"]
-    # update a user in the database by id
+
     if not is_admin(current_user) and current_user.id != user_id:
         raise HTTPException(status_code=403, detail="Not authorized to update this user")
 
+    user_collection = db['users']
     old_user = user_collection.find_one_and_update({"_id": user_id}, {"$set": user_info_to_update})
     if old_user is None:
         raise HTTPException(status_code=404, detail=f"User with id {user_id} not found")
@@ -114,11 +89,12 @@ async def update_user(user_id: PyObjectId, updated_info: UpdateUser, current_use
 
 
 @router.delete("/delete/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id: PyObjectId, current_user: User = Depends(get_current_user)):
+async def delete_user(user_id: PyObjectId, current_user: User = Depends(get_current_user), db=Depends(get_db)):
     """Delete a user from the database by id"""
     if not is_admin(current_user) and current_user.id != user_id:
         raise HTTPException(status_code=403, detail="Not authorized to delete this user")
 
+    user_collection = db['users']
     user = user_collection.find_one_and_delete({"_id": user_id})
 
     if user is None:
