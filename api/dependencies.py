d5691e1506baa352961b1361a84d8dd301628fb2
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-08-28 23:31:25 -0700

    Moved the get_current_user() function to the dependencies file

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,6 +1,17 @@
+import jwt
 from pymongo import MongoClient
 import dotenv
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
+from models import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = dotenv.get_key(".env", "SECRET_KEY")
+ALGORITHM = "HS256"
 
 def get_db():
     mongo_host = dotenv.get_key(".env", "MONGO_HOST")
@@ -8,3 +19,24 @@ def get_db():
     db = client['lingua-tile']
     return db
 
+
+def get_current_user(token: str = Depends(oauth2_scheme), db=Depends(get_db)):
+    try:
+        # Decode the token to get the username
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+
+        user_collection = db['users']
+
+        # Find the user in the database by username
+        user = user_collection.find_one({"username": username})
+        if user is None:
+            raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+
+        return User(**user)
+
+    except jwt.PyJWTError:
+        raise HTTPException(status_code=401, detail="Invalid authentication credentials")
