d61b6d8a0488a8293bb3e04618b196113b661adf
zliel <47030040+zliel@users.noreply.github.com>
2025-12-28 12:59:13 -0800

    Added pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,54 @@
+import pytest
+import pytest_asyncio
+from httpx import AsyncClient, ASGITransport
+from app.main import app
+from app.config import get_settings
+from api import dependencies
+from pymongo import AsyncMongoClient
+import os
+
+
+@pytest_asyncio.fixture
+async def client():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as c:
+        yield c
+
+
+@pytest.fixture
+def settings():
+    return get_settings()
+
+
+@pytest_asyncio.fixture
+async def db_client(settings):
+    if settings.MONGO_HOST:
+        client = AsyncMongoClient(settings.MONGO_HOST)
+        yield client
+        await client.close()
+    else:
+        yield None
+
+
+@pytest_asyncio.fixture
+async def db(db_client):
+    if db_client:
+        return db_client["lingua-tile-test"]
+    return None
+
+
+@pytest_asyncio.fixture(autouse=True)
+async def override_dependency(db_client):
+    if db_client:
+        app.state.mongo_client = db_client
+        dependencies.db_client = db_client
+        pass
+
+    yield
+
+    if db_client:
+        await db_client["lingua-tile-test"].users.delete_many({})
+        await db_client["lingua-tile-test"].lessons.delete_many({})
+        await db_client["lingua-tile-test"].cards.delete_many({})
+        await db_client["lingua-tile-test"].lesson_reviews.delete_many({})
