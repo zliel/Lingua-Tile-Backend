ea3ccf1b52c46da8323a21caa77da7e0d9fcf56b
zliel <47030040+zliel@users.noreply.github.com>
2025-12-31 00:05:53 -0800

    Updated the logging to add in context variables and updated filters

--- app/logging_config.py
+++ app/logging_config.py
@@ -3,9 +3,22 @@ import sys
 from pythonjsonlogger.jsonlogger import JsonFormatter
 
 
+from app.context import get_request_id
+
+
+class ContextFilter(logging.Filter):
+    """
+    Filter that injects the request_id context variable into the log record.
+    """
+
+    def filter(self, record):
+        record.request_id = get_request_id()
+        return True
+
+
 def setup_logging():
     formatter = JsonFormatter(
-        "%(asctime)s %(levelname)s %(name)s %(module)s %(message)s",
+        "%(asctime)s %(levelname)s %(name)s %(message)s %(request_id)s",
         rename_fields={"asctime": "timestamp", "levelname": "level"},
         datefmt="%Y-%m-%dT%H:%M:%S%z",
     )
@@ -13,10 +26,14 @@ def setup_logging():
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(formatter)
 
+    context_filter = ContextFilter()
+    handler.addFilter(context_filter)
+
     # List of loggers to configure
     loggers = [
         logging.getLogger(),
         logging.getLogger("uvicorn.error"),
+        logging.getLogger("uvicorn.access"),
     ]
 
     for logger in loggers:
